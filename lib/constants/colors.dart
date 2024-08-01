a8bdde401556acc394b30cd90e6c130931463425
Abhi00315 <abhinav.saravanakumar@gmail.com>
2024-08-01 15:26:56 +0530

    Final Commit

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -9,3 +9,4 @@ const Color taskPendingColor = Color(0xFF1E88E5); // Cool blue
 
 const Color textColor = Color(0xFF212121); // Dark grey
 const Color hintColor = Color(0xFF9E9E9E); // Light grey
+//
